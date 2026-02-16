a016fa285461ce65790a1c5594f5c327d6a07b04
Dubaki <shaodubaki@gmail.com>
2026-02-16 22:53:05 +0500

    feat: update to 20 verified TON NFT gifts from Fragment (Snoop Dogg, UFC, premium collections)

--- backend/seed_data.py
+++ backend/seed_data.py
@@ -1,45 +1,32 @@
 # Seed data for gifts catalog
-# Real TON NFT gifts from Fragment and other marketplaces
+# Real TON NFT gifts verified on Fragment.com (February 2026)
 
 GIFTS_SEED_DATA = [
-    # First wave gifts
-    {"name": "Delicious Cake", "slug": "deliciouscake", "total_supply": None},
-    {"name": "Green Circle", "slug": "greencircle", "total_supply": None},
-    {"name": "Milk Coffee", "slug": "milkcoffee", "total_supply": None},
-    {"name": "Blue Star", "slug": "bluestar", "total_supply": None},
-    {"name": "Red Ball", "slug": "redball", "total_supply": None},
-    {"name": "Lollipop", "slug": "lollipop", "total_supply": None},
-    {"name": "Pizza", "slug": "pizza", "total_supply": None},
-    {"name": "Ice Cream", "slug": "icecream", "total_supply": None},
-    {"name": "Champagne", "slug": "champagne", "total_supply": None},
-    {"name": "Party Popper", "slug": "partypopper", "total_supply": None},
-
-    # Seasonal gifts
-    {"name": "Black Rabbit", "slug": "blackrabbit", "total_supply": None},
-    {"name": "Christmas Tree", "slug": "christmastree", "total_supply": None},
-    {"name": "Snowman", "slug": "snowman", "total_supply": None},
-    {"name": "Envelope", "slug": "envelope", "total_supply": None},
-    {"name": "Dancing heart", "slug": "dancingheart", "total_supply": None},
-    {"name": "Chocolate box", "slug": "chocolatebox", "total_supply": None},
-    {"name": "Teddy bear", "slug": "teddybear", "total_supply": None},
-    {"name": "Flowers", "slug": "flowers", "total_supply": None},
-    {"name": "Dragon", "slug": "dragon", "total_supply": None},
-    {"name": "Heart Locket", "slug": "heartlocket", "total_supply": None},
-    {"name": "Loot Bag", "slug": "lootbag", "total_supply": None},
-    {"name": "Jingle Bells", "slug": "jinglebells", "total_supply": None},
-
-    # Premium gifts
-    {"name": "Westside Sign", "slug": "westsidesign", "total_supply": None},
-    {"name": "Toy Bear", "slug": "toybear", "total_supply": None},
-    {"name": "Genie Lamp", "slug": "genielamp", "total_supply": None},
-    {"name": "Bling Binky", "slug": "blingbinky", "total_supply": None},
+    # Premium collection gifts
     {"name": "Heroic Helmet", "slug": "heroichelmet", "total_supply": None},
-    {"name": "Astral Shard", "slug": "astralshard", "total_supply": None},
-    {"name": "Perfume Bottle", "slug": "perfumebottle", "total_supply": None},
-    {"name": "Mighty Arm", "slug": "mightyarm", "total_supply": None},
+    {"name": "Nail Bracelet", "slug": "nailbracelet", "total_supply": None},
+    {"name": "Loot Bag", "slug": "lootbag", "total_supply": None},
     {"name": "Swiss Watch", "slug": "swisswatch", "total_supply": None},
-    {"name": "Signet Ring", "slug": "signetring", "total_supply": None},
-    {"name": "Vintage Cigar", "slug": "vintagecigar", "total_supply": None},
+    {"name": "Perfume Bottle", "slug": "perfumebottle", "total_supply": None},
+
+    # Rare gems and collectibles
+    {"name": "Ion Gem", "slug": "iongem", "total_supply": None},
+    {"name": "Mini Oscar", "slug": "minioscar", "total_supply": None},
+    {"name": "Gem Signet", "slug": "gemsignet", "total_supply": None},
+    {"name": "Westside Sign", "slug": "westidesign", "total_supply": None},  # Snoop Dogg collection
+    {"name": "Magic Potion", "slug": "magicpotion", "total_supply": None},
+
+    # Special event gifts
+    {"name": "UFC Box", "slug": "ufcbox", "total_supply": None},  # UFC/Khabib collection
+    {"name": "Bonded Ring", "slug": "bondedring", "total_supply": None},
+    {"name": "Genie Lamp", "slug": "genielamp", "total_supply": None},
     {"name": "Scared Cat", "slug": "scaredcat", "total_supply": None},
-    {"name": "Voodoo Doll", "slug": "voodoodoll", "total_supply": None},
+    {"name": "Sharp Tongue", "slug": "sharptongue", "total_supply": None},
+
+    # Seasonal and themed gifts
+    {"name": "Easter Egg", "slug": "easteregg", "total_supply": None},
+    {"name": "Kissed Frog", "slug": "kissedfrog", "total_supply": None},
+    {"name": "Electric Skull", "slug": "electricskull", "total_supply": None},
+    {"name": "Low Rider", "slug": "lowrider", "total_supply": None},  # Snoop Dogg collection
+    {"name": "Neko Helmet", "slug": "nekohelmet", "total_supply": None},
 ]
